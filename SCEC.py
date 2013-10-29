91d280bd3ee1ce43d1a350077d52847d0c5ee699
Jie Zhang <jzhang980@berkeley.edu>
2013-10-29 09:22:48 -0700

    add SCEC notebook

---
+++ SCEC.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <codecell>
+
+import urllib
+url= 'http://www.data.scec.org/ftp/catalogs/SCEC_DC/SCEDC_catalogs.tar.gz'
+urllib.urlretrieve(url, 'SCED_catalogs.tar.gz')
+
+# <codecell>
+
+
